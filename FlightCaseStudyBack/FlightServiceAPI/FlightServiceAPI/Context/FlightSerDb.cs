ecd2e3e4489cd1dce1d6ba1aba8aef555c1f1969
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2021-09-15 11:52:46 +0530

    Add files via upload

---
+++ FlightCaseStudyBack/FlightServiceAPI/FlightServiceAPI/Context/FlightSerDb.cs
@@ -0,0 +1,20 @@
+﻿using FlightServiceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightServiceAPI.Context
+{
+    public class FlightSerDb : DbContext
+    {
+        public FlightSerDb(DbContextOptions<FlightSerDb> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Airline> Airlines { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
